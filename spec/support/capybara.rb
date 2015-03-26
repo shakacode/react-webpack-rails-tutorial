2be61517a4b3a03c76a46b52d47fedf88b548a0e
Dylan Grafmyre <dgrafmyre@gmail.com>
2015-03-26 06:41:08 -1000

    Details and links why webkit and poltergeist are not used

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,16 +1,14 @@
 require 'selenium-webdriver'
 
+# At this time Capybara-Webkit and Poltergeist do not work as headless drivers with Capybara
+# and ReactJS without custom javascript implementation and modified css.
+# https://github.com/thoughtbot/capybara-webkit/issues/494
+# https://github.com/teampoltergeist/poltergeist/issues/292
+
 Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
 
-# Saves page to place specfied at
-# NOTE: you must pass js: true for the feature definition (or else you'll see that render doesn't exist!)
-# call force = true, or set ENV[RENDER_SCREENSHOTS] == 'YES'
-def render_page(name, force = false)
-  if force || (ENV['RENDER_SCREENSHOTS'] == 'YES')
-    path = File.join Rails.application.config.integration_test_render_dir, "#{name}.png"
-    page.driver.render(path)
-  end
-end
+
+
