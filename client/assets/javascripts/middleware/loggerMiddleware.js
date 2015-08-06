42e6cda7116184075d981143e7802a6544ae6c16
Justin Gordon <justin.gordon@gmail.com>
2015-08-06 02:30:14 -0700

    WIP for move to redux

---
+++ client/assets/javascripts/middleware/loggerMiddleware.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+
+function logger({ getState }) {
+  return (next) => (action) => {
+    console.log('will dispatch', action);
+
+    // Call the next dispatch method in the middleware chain.
+    let returnValue = next(action);
+
+    console.log('state after dispatch', getState());
+
+    // This will likely be the action itself, unless
+    // a middleware further in chain changed it.
+    return returnValue;
+  };
+}
--- client/assets/javascripts/reducers/commentReducer.js
+++ client/assets/javascripts/reducers/commentReducer.js
@@ -1,27 +1,31 @@
 import * from '../constants/ActionTypes';
 
-const initialState = {
+import Immutable from 'immutable';
+
+const initialState = Immutable.Map({
   comments: [],
   ajaxCounter: 0,
   fetchCommentError: '',
-  submitCommentError: ''
-};
+  submitCommentError: '',
+});
 
 export default function commentReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_COMMENTS_SUCCESS:
-      return Object.assign({}, state, { comments: action.comments, fetchCommentError: '' });
+      return state.merge({ comments: action.comments, fetchCommentError: '' });
     case FETCH_COMMENTS_FAILURE:
-      return Object.assign({}, state, { fetchCommentError: action.error });
+      return state.merge({ fetchCommentError: action.error });
     case SUBMIT_COMMENT_SUCCESS:
-      return Object.assign({}, state,
-        { comments: action.comments.unshift(action.comment), submitCommentError: '' });
+      return state.withMutatations(mState => {
+        mState.updateIn(['comments'], comments => comments.unshift(action.comment)).
+          merge({ submitCommentError: '' });
+      });
     case SUBMIT_COMMENT_FAILURE:
-      return Object.assign({}, state, { submitCommentError: action.error });
+      return state.merge({ submitCommentError: action.error });
     case INCREMENT_AJAX_COUNTER:
-      return Object.assign({}, state, { ajaxCounter: state.ajaxCounter + 1 });
+      return state.merge({ ajaxCounter: state.get('ajaxCounter') + 1 });
     case DECREMENT_AJAX_COUNTER:
-      return Object.assign({}, state, { ajaxCounter: state.ajaxCounter - 1 });
+      return state.merge({ ajaxCounter: state.get('ajaxCounter') - 1 });
     default:
       return state;
   }
