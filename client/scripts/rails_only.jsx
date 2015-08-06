a55b9150c99c4bb18d6cf4766ab8d91a4e8e5948
Justin Gordon <justin.gordon@gmail.com>
2015-08-06 16:36:10 -0700

    Fixed failing tests
    * For the phantomjs issue: https://github.com/ariya/phantomjs/issues/12401
    * Still getting a warning
      iterable.length has been deprecated, use iterable.size or iterable.count(). This warning will become a silent error in a future version. Error:
        at :4602

--- client/scripts/rails_only.jsx
+++ client/scripts/rails_only.jsx
@@ -4,3 +4,7 @@
 // https://facebook.github.io/react/docs/working-with-the-browser.html
 require('es5-shim/es5-shim');
 require('es5-shim/es5-sham');
+
+// Due to issue: https://github.com/ariya/phantomjs/issues/12401
+// Phantomjs does not like promises
+require('es6-promise').polyfill();
