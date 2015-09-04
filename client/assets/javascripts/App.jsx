73dde44ba6fa456cddb6a5bf91f93f1e0d1bae28
Josias Schneider <josias.ds@gmail.com>
2015-09-04 18:58:31 -0300

    Demonstrate react-router

--- client/assets/javascripts/App.jsx
+++ client/assets/javascripts/App.jsx
@@ -2,16 +2,18 @@ import $ from 'jquery';
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { history } from 'react-router/lib/BrowserHistory';
 
-import CommentScreen from './components/CommentScreen';
 import CommentStore from './stores/CommentStore';
+import routes from './routes/routes';
 
 $(function onLoad() {
   function render() {
     if ($('#content').length > 0) {
       React.render((
         <Provider store={CommentStore}>
-          { () => <CommentScreen /> }
+          { () => <Router history={history} children={routes} /> }
         </Provider>
       ), document.getElementById('content'));
     }
