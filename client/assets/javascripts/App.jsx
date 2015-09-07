b2c683358199565d8eb298b221fb92bc99c29561
Mario Perez <mapreal19@gmail.com>
2015-09-07 10:57:34 +0200

    Using react_on_rails gem for rendering components on the client
    - Paired with @samnang

--- client/assets/javascripts/App.jsx
+++ client/assets/javascripts/App.jsx
@@ -1,26 +1,14 @@
-import $ from 'jquery';
-
 import React from 'react';
 import { Provider } from 'react-redux';
 
 import CommentScreen from './components/CommentScreen';
 import CommentStore from './stores/CommentStore';
 
-$(function onLoad() {
-  function render() {
-    if ($('#content').length > 0) {
-      React.render((
-        <Provider store={CommentStore}>
-          { () => <CommentScreen /> }
-        </Provider>
-      ), document.getElementById('content'));
-    }
-  }
-
-  render();
-
-  // Next part is to make this work with turbo-links
-  $(document).on('page:change', () => {
-    render();
-  });
-});
+window.App = () => {
+  const reactComponent = (
+    <Provider store={CommentStore}>
+      {() => <CommentScreen />}
+    </Provider>
+  );
+  return reactComponent;
+};
