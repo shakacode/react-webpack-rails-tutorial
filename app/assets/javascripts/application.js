361f4338ebb39a5d3934b00cb6d6fcf494773000
dylangrafmyre <dgrafmyre@gmail.com>
2015-09-28 22:02:23 -1000

    update react_on_rails gem to latest 0.1.8

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 
 // Need to be on top to allow Poltergeist test to work with React.
 //= require es5-shim/es5-shim
+//= require react_on_rails
 
 // It is important that generated/vendor-bundle must be before bootstrap since it is exposing jQuery and jQuery-ujs
 //= require generated/vendor-bundle
