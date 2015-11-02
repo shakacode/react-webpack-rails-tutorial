36167f265480154e92e1c9cdd16462cf59e9b0d6
Justin Gordon <justin.gordon@gmail.com>
2015-11-01 20:13:37 -0800

    Trying out all the assets configuration diff spot
    config.assets.paths << Rails.root.join("client", "assets", "stylesheets")

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,9 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
+# Add client/assets/stylesheets to asset pipeline's search path.
+Rails.application.config.assets.paths << Rails.root.join("client", "assets", "stylesheets")
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( generated/server-bundle.js )
