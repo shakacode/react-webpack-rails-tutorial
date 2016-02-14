5f433a3fc6605c879f579fca9ca21ea70acb25e3
Justin Gordon <justin.gordon@gmail.com>
2016-02-14 13:48:19 -0800

    Update link highlight

--- app/assets/javascripts/rails_startup.js
+++ app/assets/javascripts/rails_startup.js
@@ -1,4 +1,4 @@
-$(document).on('ready page:load', function () {
+$(document).on('ready turbolinks:load', function () {
   // highlight active page in the top menu
   $('nav a').parents('li,ul').removeClass('active');
   $('nav a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
