c5dfe3d6a69ad92c37e75d2b9d60f4c7dc5dac09
Aaron Reisman <aaron@lifeiscontent.net>
2016-02-15 14:37:50 -0800

    Update navigationbar to be a react component

--- app/assets/javascripts/rails_startup.js
+++
@@ -1,5 +0,0 @@
-$(document).on('ready turbolinks:load', function () {
-  // highlight active page in the top menu
-  $('nav a').parents('li,ul').removeClass('active');
-  $('nav a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
-});
--- client/app/bundles/comments/startup/clientRegistration.jsx
+++ client/app/bundles/comments/startup/clientRegistration.jsx
@@ -1,5 +1,6 @@
 import App from './ClientApp';
 import RouterApp from './ClientRouterApp';
+import Navigationbar from '../components/Navigationbar/Navigationbar';
 import SimpleCommentScreen from '../components/SimpleCommentScreen/SimpleCommentScreen';
 import ReactOnRails from 'react-on-rails';
 
@@ -12,5 +13,6 @@ ReactOnRails.register(
     App,
     RouterApp,
     SimpleCommentScreen,
+    Navigationbar
   }
 );
