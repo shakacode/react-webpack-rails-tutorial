fc5dc69326b651d4441f4bf568091e48d96686ab
Aaron Reisman <aaron@lifeiscontent.net>
2016-02-15 18:21:36 -0800

    Update navigationbar to be a react component

--- client/app/bundles/comments/startup/clientRegistration.jsx
+++ client/app/bundles/comments/startup/clientRegistration.jsx
@@ -1,5 +1,6 @@
 import App from './ClientApp';
 import RouterApp from './ClientRouterApp';
+import Navigationbar from '../components/Navigationbar/Navigationbar';
 import SimpleCommentScreen from '../components/SimpleCommentScreen/SimpleCommentScreen';
 import ReactOnRails from 'react-on-rails';
 
@@ -7,10 +8,9 @@ ReactOnRails.setOptions({
   traceTurbolinks: TRACE_TURBOLINKS, // eslint-disable-line no-undef
 });
 
-ReactOnRails.register(
-  {
-    App,
-    RouterApp,
-    SimpleCommentScreen,
-  }
-);
+ReactOnRails.register({
+  App,
+  RouterApp,
+  SimpleCommentScreen,
+  Navigationbar,
+});
