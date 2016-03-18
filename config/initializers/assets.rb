61812c5b7ee904c2ca84c904b43f40de0d853ce6
Justin Gordon <justin.gordon@gmail.com>
2016-03-17 22:42:39 -1000

    Make assets.rb cleaner

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,11 @@ Rails.application.config.assets.paths << Rails.root.join("app", "assets", "webpa
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w(
-  application_*.*
-  server-bundle.js
-)
+Rails.application.config.assets.precompile << "server-bundle.js"
+
+type = ENV["REACT_ON_RAILS_ENV"] == "HOT" ? "non_webpack" : "static"
+Rails.application.config.assets.precompile +=
+  [
+    "application_#{type}.js",
+    "application_#{type}.css"
+  ]
