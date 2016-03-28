312bf19ec17fff8dd2fa11b1950fe0c682f844ec
Yoochan Seo <supergnee@gmail.com>
2016-03-28 22:51:48 +0900

    Display Redux Monitor in a new window

---
+++ client/app/bundles/comments/containers/showDevTools.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import DevTools from './DevTools';
+
+export default function showDevTools(store) {
+  const popup = window.open(null, 'Redux DevTools',
+  'menubar=no,location=no,resizable=yes,scrollbars=no,status=no');
+
+  // Reload in case it already exists
+  popup.location.reload();
+
+  setTimeout(() => {
+    popup.document.write('<div id="react-devtools-root"></div>');
+    render(
+      <DevTools store={store} />,
+      popup.document.getElementById('react-devtools-root')
+    );
+  }, 10);
+}
--- client/app/bundles/comments/startup/ServerApp.jsx
+++ client/app/bundles/comments/startup/ServerApp.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
 import createStore from '../store/commentsStore';
 import NonRouterCommentsContainer from '../containers/NonRouterCommentsContainer';
 
 export default props => {
   const store = createStore(props);
+
+  if (process.env.NODE_ENV !== 'production') {
+    const showDevTools = require('../containers/showDevTools');
+    showDevTools(store);
+  }
+
   return (
       <Provider store={store}>
         <NonRouterCommentsContainer />
