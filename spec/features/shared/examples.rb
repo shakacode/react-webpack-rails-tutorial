e017384b14a8e71c60d6bac88e4f80f0ef6dc0ef
Justin Gordon <justin.gordon@gmail.com>
2016-07-12 22:01:14 -1000

    Misc changes

--- spec/features/shared/examples.rb
+++ spec/features/shared/examples.rb
@@ -10,7 +10,7 @@ shared_examples "New Comment Submission" do
     scenario "comment is added" do
       expect(page).to have_css(".js-comment-author", text: name)
       expect(page).to have_css(".js-comment-text", text: text)
-      expect(page).to have_css(".js-comment-count",
+      expect(page).to have_css("#js-comment-count",
                                text: "Comments: #{Comment.count}")
     end
   end
@@ -20,7 +20,7 @@ shared_examples "New Comment Submission" do
 
     scenario "comment is not added" do
       expect(page).to have_selector(".comment", count: comments_count)
-      expect(page).to have_css(".js-comment-count",
+      expect(page).to have_css("#js-comment-count",
                                text: "Comments: #{Comment.count}")
     end
   end
