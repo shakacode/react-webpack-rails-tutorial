a65fb5e8bbfd3094bead552dd08f3ba83a020dc5
Justin Gordon <justin.gordon@gmail.com>
2016-08-01 19:40:28 -1000

    Fix express server rendering
    * Had to work around no shared store support
    * See https://github.com/shakacode/react_on_rails/issues/504

---
+++ client/app/bundles/comments/startup/ClientRouterAppExpress.jsx
@@ -0,0 +1,33 @@
+// Compare to ../ServerRouterApp.jsx
+import React from 'react';
+import { Provider } from 'react-redux';
+import ReactOnRails from 'react-on-rails';
+import { Router, browserHistory } from 'react-router';
+import routes from '../routes/routes';
+import { syncHistoryWithStore } from 'react-router-redux';
+
+// Because of https://github.com/shakacode/react_on_rails/issues/504
+// we need to skip using a shared store for the express server startup.
+import createStore from '../store/routerCommentsStore';
+
+const RouterAppExpress = (_props, _railsContext) => {
+
+  // See comment above
+  const store = createStore(_props);
+
+  // Create an enhanced history that syncs navigation events with the store
+  const history = syncHistoryWithStore(
+    browserHistory,
+    store
+  );
+
+  return (
+    <Provider store={store}>
+      <Router history={history} children={routes} />
+    </Provider>
+  );
+};
+
+ReactOnRails.register({
+  RouterAppExpress,
+});
