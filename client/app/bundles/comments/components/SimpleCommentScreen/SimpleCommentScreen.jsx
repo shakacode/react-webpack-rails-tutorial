9c924d4fbe931efd10bd737d93711c42166edfb0
Sergey Tarasov <dzirtusss@gmail.com>
2016-08-11 17:36:28 +0300

    updated to use ReactOnRails.authenticityHeader()

--- client/app/bundles/comments/components/SimpleCommentScreen/SimpleCommentScreen.jsx
+++ client/app/bundles/comments/components/SimpleCommentScreen/SimpleCommentScreen.jsx
@@ -2,98 +2,96 @@ import React from 'react';
 import Immutable from 'immutable';
 import request from 'axios';
 import _ from 'lodash';
+import ReactOnRails from 'react-on-rails';
 
 import BaseComponent from 'libs/components/BaseComponent';
-import metaTagsManager from 'libs/metaTagsManager';
 
 import CommentForm from '../CommentBox/CommentForm/CommentForm';
 import CommentList from '../CommentBox/CommentList/CommentList';
 import css from './SimpleCommentScreen.scss';
 
 export default class SimpleCommentScreen extends BaseComponent {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      $$comments: Immutable.fromJS([]),
-      isSaving: false,
-      fetchCommentsError: null,
-      submitCommentError: null,
-    };
-
-    _.bindAll(this, '_fetchComments', '_handleCommentSubmit');
-  }
-
-  componentDidMount() {
-    this._fetchComments();
-  }
-
-  _fetchComments() {
-    return (
-      request
-        .get('comments.json', { responseType: 'json' })
-        .then(res => this.setState({ $$comments: Immutable.fromJS(res.data.comments) }))
-        .catch(error => this.setState({ fetchCommentsError: error }))
-    );
-  }
-
-  _handleCommentSubmit(comment) {
-    this.setState({ isSaving: true });
-
-    const requestConfig = {
-      responseType: 'json',
-      headers: {
-        'X-CSRF-Token': metaTagsManager.getCSRFToken(),
-      },
-    };
-
-    return (
-      request
-        .post('comments.json', { comment }, requestConfig)
-        .then(() => {
-          const { $$comments } = this.state;
-          const $$comment = Immutable.fromJS(comment);
-
-          this.setState({
-            $$comments: $$comments.unshift($$comment),
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            $$comments: Immutable.fromJS([]),
             isSaving: false,
-          });
-        })
-        .catch(error => {
-          this.setState({
-            submitCommentError: error,
-            isSaving: false,
-          });
-        })
-    );
-  }
+            fetchCommentsError: null,
+            submitCommentError: null,
+        };
 
-  render() {
-    const cssTransitionGroupClassNames = {
-      enter: css.elementEnter,
-      enterActive: css.elementEnterActive,
-      leave: css.elementLeave,
-      leaveActive: css.elementLeaveActive,
-    };
+        _.bindAll(this, '_fetchComments', '_handleCommentSubmit');
+    }
 
-    return (
-      <div className="commentBox container">
-        <h2>Comments</h2>
-        <p>
-          Text take Github Flavored Markdown. Comments older than 24 hours are deleted.<br />
-          <b>Name</b> is preserved. <b>Text</b> is reset, between submits.
-        </p>
-        <CommentForm
-          isSaving={this.state.isSaving}
-          actions={{ submitComment: this._handleCommentSubmit }}
-          error={this.state.submitCommentError}
-          cssTransitionGroupClassNames={cssTransitionGroupClassNames}
-        />
-        <CommentList
-          $$comments={this.state.$$comments}
-          error={this.state.fetchCommentsError}
-          cssTransitionGroupClassNames={cssTransitionGroupClassNames}
-        />
-      </div>
-    );
-  }
+    componentDidMount() {
+        this._fetchComments();
+    }
+
+    _fetchComments() {
+        return (
+            request
+                .get('comments.json', { responseType: 'json' })
+                .then(res => this.setState({ $$comments: Immutable.fromJS(res.data.comments) }))
+                .catch(error => this.setState({ fetchCommentsError: error }))
+        );
+    }
+
+    _handleCommentSubmit(comment) {
+        this.setState({ isSaving: true });
+
+        const requestConfig = {
+            responseType: 'json',
+            headers: ReactOnRails.authenticityHeader(),
+        };
+
+        return (
+            request
+                .post('comments.json', { comment }, requestConfig)
+                .then(() => {
+                    const { $$comments } = this.state;
+                    const $$comment = Immutable.fromJS(comment);
+
+                    this.setState({
+                        $$comments: $$comments.unshift($$comment),
+                        isSaving: false,
+                    });
+                })
+                .catch(error => {
+                    this.setState({
+                        submitCommentError: error,
+                        isSaving: false,
+                    });
+                })
+        );
+    }
+
+    render() {
+        const cssTransitionGroupClassNames = {
+            enter: css.elementEnter,
+            enterActive: css.elementEnterActive,
+            leave: css.elementLeave,
+            leaveActive: css.elementLeaveActive,
+        };
+
+        return (
+            <div className="commentBox container">
+                <h2>Comments</h2>
+                <p>
+                    Text take Github Flavored Markdown. Comments older than 24 hours are deleted.<br />
+                    <b>Name</b> is preserved. <b>Text</b> is reset, between submits.
+                </p>
+                <CommentForm
+                    isSaving={this.state.isSaving}
+                    actions={{ submitComment: this._handleCommentSubmit }}
+                    error={this.state.submitCommentError}
+                    cssTransitionGroupClassNames={cssTransitionGroupClassNames}
+                />
+                <CommentList
+                    $$comments={this.state.$$comments}
+                    error={this.state.fetchCommentsError}
+                    cssTransitionGroupClassNames={cssTransitionGroupClassNames}
+                />
+            </div>
+        );
+    }
 }
