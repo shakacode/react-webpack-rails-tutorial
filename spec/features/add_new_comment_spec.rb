585de8d3963ea9544122346233b266db1daffc37
AlexKVal <alexkval@gmail.com>
2016-08-19 22:22:04 +0300

    Add Capybara specs

--- spec/features/add_new_comment_spec.rb
+++ spec/features/add_new_comment_spec.rb
@@ -6,36 +6,45 @@ feature "Add new comment" do
   context "React Router", page: :main, js: true do
     context "via Horizontal Form", form: :horizontal do
       include_examples "New Comment Submission", true
+      include_examples "Validation errors displaying"
     end
     context "via Inline Form", form: :inline do
       include_examples "New Comment Submission", true
+      include_examples "Validation errors displaying"
     end
     context "via Stacked Form", form: :stacked do
       include_examples "New Comment Submission", true
+      include_examples "Validation errors displaying"
     end
   end
 
   context "React/Redux", page: :react_demo, js: true do
     context "via Horizontal Form", form: :horizontal do
       include_examples "New Comment Submission", true
+      include_examples "Validation errors displaying"
     end
     context "via Inline Form", form: :inline do
       include_examples "New Comment Submission", true
+      include_examples "Validation errors displaying"
     end
     context "via Stacked Form", form: :stacked do
       include_examples "New Comment Submission", true
+      include_examples "Validation errors displaying"
     end
   end
 
   context "simple page", page: :simple, js: true do
     context "via Horizontal Form", form: :horizontal do
       include_examples "New Comment Submission", false
+      include_examples "Validation errors displaying"
     end
     context "via Inline Form", form: :inline do
       include_examples "New Comment Submission", false
+      include_examples "Validation errors displaying"
     end
     context "via the Stacked Form", form: :stacked, driver: js_selenium_driver do
       include_examples "New Comment Submission", false
+      include_examples "Validation errors displaying"
     end
   end
 
