a31bc9070daaf84f22aea1d6a81db6301254d210
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2016-11-02 21:17:42 -0700

    Add configuration for code splitting

---
+++ client/webpack.client.rails.splitting.config.js
@@ -0,0 +1,12 @@
+// Webpack configuration for code splitting
+
+const config = require('./webpack.client.rails.build.config');
+
+// Add publicPath key to output so that webpack know where to fetch
+// the code chunks from.
+config.output.publicPath = "/assets/"
+
+// Use the code splitting entry point
+config.entry.app = [
+  './app/bundles/comments/startup/clientRegistrationCodeSplitting'
+];
