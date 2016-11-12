5cc95d3f624441f3766f9eebcdddc5b978faafb6
jasonych99 <jasonych99@gmail.com>
2016-11-12 17:40:34 +0100

    Frontend: add i18n-js to webpack

--- client/webpack.client.base.config.js
+++ client/webpack.client.base.config.js
@@ -36,6 +36,7 @@ module.exports = {
       'react-on-rails',
       'react-router-redux',
       'redux-thunk',
+      'i18n-js',
     ],
 
     // This will contain the app entry points defined by webpack.hot.config and webpack.rails.config
