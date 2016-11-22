b031e1c568acd852c6d2f72532f2a866a421f780
Alexey Karasev <karasev.alexey@gmail.com>
2016-11-22 16:32:50 +0100

    added list view styling

---
+++ mobile/ReactNativeTutorial/app/bundles/comments/components/Index/List/Item/ItemStyle.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    minHeight: 80,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    backgroundColor: '#F0F0F0',
+    shadowColor: '#000000',
+    shadowRadius: 11,
+    shadowOpacity: 0.25,
+    shadowOffset: {
+      height: 2,
+      width: 0,
+    },
+  },
+  header: {
+    marginVertical: 5,
+    fontWeight: 'bold',
+    fontSize: 17,
+    textAlign: 'center',
+  },
+  body: {
+    marginHorizontal: 10,
+    marginBottom: 10,
+    color: '#555555',
+  }
+});
+
+export default styles;
--- mobile/ReactNativeTutorial/app/bundles/comments/components/Index/List/List.js
+++ mobile/ReactNativeTutorial/app/bundles/comments/components/Index/List/List.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { ListView } from 'react-native';
+import Item from './Item/Item';
 
 import styles from './ListStyle';
 
@@ -8,14 +9,36 @@ type PropsType = {
 
 }
 
-const List = (props: PropsType) => (
-  <ListView
-    style={styles.container}
-    dataSource={new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })}
-    renderRow={() => null}
-  >
-  </ListView>
-);
+class List extends React.Component {
+
+  props: PropsType;
+
+  state: {
+    dataSource: ListView.DataSource;
+  };
+
+
+  constructor(props: PropsType) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ dataSource: this.state.dataSource.cloneWithRows(nextProps.comments) })
+  }
+
+  render() {
+    return (
+      <ListView
+        style={styles.container}
+        dataSource={this.state.dataSource}
+        renderRow={(item) => <Item {...item} />}
+      >
+      </ListView>
+    )
+  }
+}
 
 export default List;
-
