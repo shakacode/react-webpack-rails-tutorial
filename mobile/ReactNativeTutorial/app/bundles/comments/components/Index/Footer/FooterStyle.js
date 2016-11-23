a114ad1cb34a691bfc221805e274e9418a035b75
Alexey Karasev <karasev.alexey@gmail.com>
2016-11-23 21:21:18 +0100

    colors

--- mobile/ReactNativeTutorial/app/bundles/comments/components/Index/Footer/FooterStyle.js
+++ mobile/ReactNativeTutorial/app/bundles/comments/components/Index/Footer/FooterStyle.js
@@ -1,13 +1,14 @@
 import { StyleSheet } from 'react-native';
+import * as colors from 'ReactNativeTutorial/app/styles/colors';
 
 const styles = StyleSheet.create({
   container: {
     height: 70,
-    backgroundColor: '#EFEFF2',
+    backgroundColor: colors.TABBAR_BACKGROUND,
     alignItems: 'center',
     justifyContent: 'center',
     borderTopWidth: 1,
-    borderTopColor: '#D2D2D2',
+    borderTopColor: colors.BORDER,
   },
 });
 
--- mobile/ReactNativeTutorial/app/setup/Router/RouterStyle.js
+++ mobile/ReactNativeTutorial/app/setup/Router/RouterStyle.js
@@ -1,8 +1,9 @@
 import { StyleSheet } from 'react-native';
+import * as colors from 'ReactNativeTutorial/app/styles/colors';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#F7F7F7',
+    backgroundColor: colors.BACKGROUND,
     paddingTop: 66,
   },
 });
