c4fbad3e99b1922bfe557d3e6fc2a64f30e77baa
Alexey Karasev <karasev.alexey@gmail.com>
2016-11-28 18:29:02 +0100

    flow

--- mobile/ReactNativeTutorial/app/bundles/comments/components/Add/Add.js
+++ mobile/ReactNativeTutorial/app/bundles/comments/components/Add/Add.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { View } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements';
 
-import type { AddPropsType } from '../../containers/Add';
+import type { AddPropsType } from '../../hocs/withAddProps';
 
 import styles from './AddStyle';
 
