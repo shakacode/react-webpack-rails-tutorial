b362f0840f97867e47e95b87243b9e62981b3311
Alexey Karasev <karasev.alexey@gmail.com>
2016-11-29 19:32:00 +0100

    removed outdated code

--- mobile/ReactNativeTutorial/app/setup/effectCreators/real.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-shadow */
-const dispatch = ({ dispatch }) => dispatch;
-const getState = ({ getState }) => getState;
-const call = () => (f, ...args) => f(...args);
-
-export default { dispatch, getState, call };
