ed1b822e3d10a951e0c2d37ee5ac12ed64fca581
Devin Osorio <me@devinosor.io>
2016-12-18 13:32:30 -0800

    Update BaseComponent.jsx
    Hey, I'm proposing a change in the BaseComponent as it seems to be using the `PureRenderMixin` mixin. The react docs states that we should consider using React.PureComponent instead, which essentially does the same shallow compare.

--- client/app/libs/components/BaseComponent.jsx
+++ client/app/libs/components/BaseComponent.jsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-export default class BaseComponent extends React.Component {
-  shouldComponentUpdate(theArgs) {
-    return PureRenderMixin.shouldComponentUpdate.apply(this, ...theArgs);
-  }
-}
+export default class BaseComponent extends React.PureComponent {
+};
