b15b46a78e0be4177a6b9d8f4ea73e58b9d3e3dd
Richa Bhardwaj <bhardwaj.richa17@gmail.com>
2017-01-06 16:58:29 +0530

    added action_cable configuration in application

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module RailsReactTutorial
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_cable.allowed_request_origins = [Rails.application.secrets.action_cable_url]
   end
 end
