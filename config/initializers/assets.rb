bb265cd740158a1e650c91607b4b995abaf2edbc
Justin Gordon <justin.gordon@gmail.com>
2017-05-29 22:05:38 -1000

    Remove commented code in assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,17 +2,3 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
-
-# Add folder with webpack generated assets to assets.paths
-# Rails.application.config.assets.paths << Rails.root.join("public", "webpack", Rails.env)
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile << "server-bundle.js"
-
-# type = ENV["REACT_ON_RAILS_ENV"] == "HOT" ? "non_webpack" : "static"
-# Rails.application.config.assets.precompile +=
-#   [
-#     "application_#{type}.js",
-#     "application_#{type}.css"
-#   ]
