313a16aa977274f5e9ebbe69b3833f8a02d417c2
Justin Gordon <justin.gordon@gmail.com>
2017-06-03 12:59:50 -1000

    tweaks

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require "coveralls"
 Coveralls.wear!("rails") # must occur before any of your application code is required
 require "spec_helper"
 require File.expand_path("../../config/environment", __FILE__)
+
 require "rspec/rails"
 require "capybara/rspec"
 require "capybara/poltergeist"
@@ -19,11 +20,6 @@ require "database_cleaner"
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
-#
-# The following line is provided for convenience purposes. It has the downside
-# of increasing the boot-up time by auto-requiring all files in the support
-# directory. Alternatively, in the individual `*_spec.rb` files, manually
-# require only the support files necessary.
 #
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
