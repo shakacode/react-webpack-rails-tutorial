80d0c451bd19fac20279bb2d659cdd24cb6f6c42
Judah Meek <judah.meek@gmail.com>
2018-04-19 02:06:33 -0500

    get rid of more stuff

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,10 @@
 # This file is copied to spec/ when you run "rails generate rspec:install"
 ENV["RAILS_ENV"] ||= "test"
-require "coveralls"
-Coveralls.wear!("rails") # must occur before any of your application code is required
 require "spec_helper"
 require File.expand_path("../../config/environment", __FILE__)
 
 require "rspec/rails"
 require "capybara/rspec"
-require "capybara/poltergeist"
 require "capybara-screenshot/rspec"
 require "database_cleaner"
 
