5c6d92b93a2ac7bea685c0b42a8369937f63a1d1
Akansha Agnihotri <shubhiakansha96@gmail.com>
2019-03-16 01:45:01 +0530

    Update SimpleHooksCommentScreen.jsx

--- client/app/bundles/comments/components/SimpleHooksCommentScreen/SimpleHooksCommentScreen.jsx
+++ client/app/bundles/comments/components/SimpleHooksCommentScreen/SimpleHooksCommentScreen.jsx
@@ -1,56 +1,69 @@
-import { React, useState, useEffect } from "react";
-import request from "axios";
-import Immutable from "immutable";
-import _ from "lodash";
-import ReactOnRails from "react-on-rails";
-import { IntlProvider, injectIntl } from "react-intl";
+import { React, useEffect, useReducer } from 'react';
+import request from 'axios';
+import Immutable from 'immutable';
+import _ from 'lodash';
+import ReactOnRails from 'react-on-rails';
+import { IntlProvider, injectIntl } from 'react-intl';
 import BaseComponent from 'libs/components/BaseComponent';
-import SelectLanguage from "libs/i18n/selectLanguage";
-import { defaultMessages, defaultLocale } from "libs/i18n/default";
-import { translations } from "libs/i18n/translations";
+import SelectLanguage from 'libs/i18n/selectLanguage';
+import { defaultMessages, defaultLocale } from 'libs/i18n/default';
+import { translations } from 'libs/i18n/translations';
 
-import CommentForm from "../CommentBox/CommentForm/CommentForm";
-import CommentList from "../CommentBox/CommentList/CommentList";
-import css from "../SimpleCommentScreen/SimpleCommentScreen.scss";
+import CommentForm from '../CommentBox/CommentForm/CommentForm';
+import CommentList from '../CommentBox/CommentList/CommentList';
+import css from '../SimpleCommentScreen/SimpleCommentScreen.scss';
 
+
+const initialState = {
+  comments: Immutable.fromJS([]),
+  isSaving: false,
+  fetchCommentsError: null,
+  submitCommentError: null,
+};
+function commentsReducer(state, action) {
+  switch (action.type) {
+    case 'Add_Comment': return ({ ...state, comments: action.payload });
+    case 'Set_Is_Saving': return ({ ...state, isSaving: action.payload });
+    case 'Set_Fetch_Comment_Error': return ({ ...state, fetchCommentsError: action.payload });
+    case 'Submit_Comment_Error': return ({ ...state, submitCommentError: action.payload });
+    default: return state;
+  }
+}
 function SimpleHooksCommentScreen() {
-  const [comments, setComments] = useState(Immutable.fromJS([]));
-  const [isSaving, setIsSaving] = useState(false);
-  const [fetchCommentsError, setFetchCommentsError] = useState(null);
-  const [submitCommentError, setSubmitCommentError] = usestate(null);
-
-  useEffect(() => {
-    this.fetchComments();
-  });
+  const [commentsList, dispatch] = useReducer(commentsReducer, initialState);
 
   function fetchComments() {
     return request
-      .get("comments.json", { responseType: "json" })
+      .get('comments.json', { responseType: 'json' })
       .then(res =>
-        setComments(Immutable.fromJS(res.data.comments))
-      )
-      .catch(error => setFetchCommentsError(error));
+        dispatch({ type: 'Add_Comment', payload: Immutable.fromJS(res.data.comments) }))
+      .catch(error => dispatch({ type: 'Set_Fetch_Comment_Error', payload: error }));
   }
 
+  useEffect(() => {
+    fetchComments();
+  });
+
+
   function handleCommentSubmit(comment) {
-    setIsSaving(true);
+    dispatch({ type: 'Set_Is_Saving', payload: true });
 
     const requestConfig = {
-      responseType: "json",
-      headers: ReactOnRails.authenticityHeaders()
+      responseType: 'json',
+      headers: ReactOnRails.authenticityHeaders(),
     };
 
     return request
-      .post("comments.json", { comment }, requestConfig)
+      .post('comments.json', { comment }, requestConfig)
       .then(() => {
         const $$comment = Immutable.fromJS(comment);
-        setComments(comments.unshift($$comment));
-        setSubmitCommentError(null);
-        setIsSaving(false);
+        dispatch({ type: 'Add_Comment', payload: comment.unshift($$comment) });
+        dispatch({ type: 'Submit_Comment_Error', payload: null });
+        dispatch({ type: 'Set_Is_Saving', payload: false });
       })
       .catch(error => {
-        setSubmitCommentError(error)
-        setIsSaving(false);
+        dispatch({ type: 'Submit_Comment_Error', payload: error });
+        dispatch({ type: 'Set_Is_Saving', payload: false });
       });
   }
 
@@ -61,7 +74,7 @@ function SimpleHooksCommentScreen() {
     enter: css.elementEnter,
     enterActive: css.elementEnterActive,
     leave: css.elementLeave,
-    leaveActive: css.elementLeaveActive
+    leaveActive: css.elementLeaveActive,
   };
 
   return (
@@ -74,14 +87,14 @@ function SimpleHooksCommentScreen() {
         <li>{formatMessage(defaultMessages.descriptionSubmitRule)}</li>
       </ul>
       <CommentForm
-        isSaving={isSaving}
+        isSaving={commentsList.isSaving}
         actions={{ submitComment: handleCommentSubmit }}
-        error={submitCommentError}
+        error={commentsList.submitCommentError}
         cssTransitionGroupClassNames={cssTransitionGroupClassNames}
       />
       <CommentList
-        $$comments={comments}
-        error={fetchCommentsError}
+        $$comments={commentsList.comments}
+        error={commentsList.fetchCommentsError}
         cssTransitionGroupClassNames={cssTransitionGroupClassNames}
       />
     </div>
@@ -93,20 +106,14 @@ export default class I18nWrapper extends BaseComponent {
     super(props);
 
     this.state = {
-      locale: defaultLocale
+      locale: defaultLocale,
     };
 
-    _.bindAll(this, "handleSetLocale");
+    _.bindAll(this, 'handleSetLocale');
   }
 
   handleSetLocale(locale) {
     this.setState({ locale });
-  }
-
-  componentDidCatch(error, info) {
-    console.log("xxxxxxxxxxxx")
-    console.log(error, info);
-    console.log("xxxxxxxxxxxx")
   }
 
   render() {
