ff74e05b963e5f3ac89cb9959f6f72d30a693faf
Justin Gordon <justin.gordon@gmail.com>
2019-03-16 23:03:12 -1000

    update gems including webpacker to v4 (#462)
    * update gems including webpacker to v4
    * Fix CI
    * Cleanup .travis.yml

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # This will guess the User class
 FactoryBot.define do
   factory :comment do
-    author "John"
-    text "This is a comment text."
+    author { "John" }
+    text { "This is a comment text." }
   end
 end
