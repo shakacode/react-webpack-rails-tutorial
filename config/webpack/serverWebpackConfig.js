c91332fc32477958d8e32997c493b120132be05d
Gustavo Carvalho <gsc13@tuta.io>
2022-02-01 05:50:56 -0300

    Update shakapacker (#493)
    * Add and install shakapacker
    * Update Babel and Webpack config to use Shakapacker
    * Update Procfile commands
    * Update bin/webpack occurences into bin/webpacker

--- config/webpack/serverWebpackConfig.js
+++ config/webpack/serverWebpackConfig.js
@@ -1,7 +1,7 @@
 // The source code including full typescript support is available at: 
 // https://github.com/shakacode/react_on_rails_tutorial_with_ssr_and_hmr_fast_refresh/blob/master/config/webpack/serverWebpackConfig.js
 
-const { merge, config } = require('@rails/webpacker');
+const { config } = require('shakapacker');
 const commonWebpackConfig = require('./commonWebpackConfig');
 
 const webpack = require('webpack');
