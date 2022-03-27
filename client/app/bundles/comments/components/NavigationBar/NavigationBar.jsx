d1a2213958c38e99e0576db806a1cfb4dc77a11c
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-03-27 00:20:59 -0500

    Updates navigation bar to bootstrap 5 and change presentation to make it similar to previous one

--- client/app/bundles/comments/components/NavigationBar/NavigationBar.jsx
+++ client/app/bundles/comments/components/NavigationBar/NavigationBar.jsx
@@ -14,47 +14,45 @@ function NavigationBar(props) {
 
   /* eslint-disable new-cap */
   return (
-    <nav className="navbar navbar-default">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-          </button>
-          <a className="navbar-brand" href="http://www.shakacode.com">
-            ShakaCode
-          </a>
-        </div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-ligth navbar-default">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="http://www.shakacode.com">
+          ShakaCode
+        </a>
+        <button
+          type="button"
+          className="navbar-toggler collapsed"
+          data-bs-toggle="collapse"
+          data-bs-target="#bs-example-navbar-collapse-1"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav">
+          <ul className="navbar-nav me-auto">
             <li
               className={classNames({
                 active: pathname === paths.ROUTER_PATH || pathname === paths.REACT_ROUTER_PATH,
               })}
             >
-              <a href={paths.ROUTER_PATH}>React Router Demo</a>
+              <a className='nav-link' href={paths.ROUTER_PATH}>React Router Demo</a>
             </li>
             <li className={classNames({ active: pathname === paths.NO_ROUTER_PATH })}>
-              <a href={paths.NO_ROUTER_PATH}>React Demo</a>
+              <a className='nav-link' href={paths.NO_ROUTER_PATH}>React Demo</a>
             </li>
             <li className={classNames({ active: pathname === paths.SIMPLE_REACT_PATH })}>
-              <a href={paths.SIMPLE_REACT_PATH}>Simple React</a>
+              <a className='nav-link' href={paths.SIMPLE_REACT_PATH}>Simple React</a>
             </li>
             <li className={classNames({ active: pathname === paths.RAILS_PATH })}>
-              <a href={paths.RAILS_PATH}>Classic Rails</a>
+              <a className='nav-link' href={paths.RAILS_PATH}>Classic Rails</a>
             </li>
             <li>
-              <a href="https://github.com/shakacode/react-webpack-rails-tutorial">Source</a>
+              <a className='nav-link' href="https://github.com/shakacode/react-webpack-rails-tutorial">Source</a>
             </li>
             <li>
-              <a href="https://forum.shakacode.com/c/reactjs">Forum</a>
+              <a className='nav-link' href="https://forum.shakacode.com/c/reactjs">Forum</a>
             </li>
             {_.isNumber(commentsCount) && CommentsCount({ commentsCount })}
           </ul>
