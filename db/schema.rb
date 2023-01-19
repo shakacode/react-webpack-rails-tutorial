9170481f57a1ca4f758fe393ffb5b5195a9ac975
Mostafa Ahangarha <ahangarha@riseup.net>
2023-01-19 18:11:27 +0330

    Apply Rails 6.1.7.1 file changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_03_221124) do
+ActiveRecord::Schema.define(version: 2023_01_19_143748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
