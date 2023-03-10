e457dd7468c4611b471bc1151a2ea8a9df525231
Mostafa Ahangarha <ahangarha@riseup.net>
2023-03-11 01:27:08 +0330

    Fix security issues, JS peer dependencies, and some warnings (#523)
    * Upgrade bootstrap-loader
    
    * Install unmet peer dependencies
    
    * Upgrade url-loader
    
    * Upgrade redux and react-redux
    
    * Remove deprecated babel-eslint
    
    * Replace react-addons-css-transition-group pachage
    
    * Modify App component to follow ROR API
    
    * Fix warning related deprecated or old rails config

--- config/initializers/new_framework_defaults_6_1.rb
+++ config/initializers/new_framework_defaults_6_1.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # Be sure to restart your server when you modify this file.
 #
 # This file contains migration options to ease your Rails 6.1 upgrade.
@@ -43,7 +44,7 @@
 # Use new connection handling API. For most applications this won't have any
 # effect. For applications using multiple databases, this new API provides
 # support for granular connection swapping.
-# Rails.application.config.active_record.legacy_connection_handling = false
+Rails.application.config.active_record.legacy_connection_handling = false
 
 # Make `form_with` generate non-remote forms by default.
 # Rails.application.config.action_view.form_with_generates_remote_forms = false
