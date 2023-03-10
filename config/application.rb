e624f7d66b4439057853e61ceb7a6e1be8ed8ae8
Mostafa Ahangarha <ahangarha@riseup.net>
2023-03-10 23:12:13 +0330

    Fix warning related deprecated or old rails config

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module RailsReactTutorial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
 
