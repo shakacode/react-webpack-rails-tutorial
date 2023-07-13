2c1ba1ca54a02937541450e3fcb8d4e9995808fa
Mostafa Ahangarha <ahangarha@riseup.net>
2023-07-13 16:54:13 +0330

    Fix importing marked

--- client/app/controllers/comments_controller.js
+++ client/app/controllers/comments_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
-import marked from 'marked';
+import * as marked from 'marked';
 
 export default class extends Controller {
   static targets = ['commentList', 'commentAuthor', 'commentText', 'commentRefresh', 'alertDiv', 'errorList'];
