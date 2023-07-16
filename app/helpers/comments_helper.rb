9b658151e7bfdadd1d2e73ab6932fe306c93da9e
Mostafa Ahangarha <ahangarha@riseup.net>
2023-07-16 21:30:23 +0330

    Revert "Improve markdown to html helper"
    This reverts commit 711c2895ca917204f89f3176a4c9d4db4c3c54b0.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,9 +1,7 @@
 module CommentsHelper
-  MarkdownParser = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-
-  def markdown_to_html(content)
+  def markdown(content)
     return '' unless content.present?
-
-    MarkdownParser.render(content).html_safe
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    @markdown.render(content).html_safe
   end
 end
