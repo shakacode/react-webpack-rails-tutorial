42236474e4cda00acadd8665791a1a014fac4f5f
yassa <yassatamer99@gmail.com>
2023-08-04 17:49:35 +0300

    Solve the marked warnings

--- client/app/bundles/comments/components/CommentBox/CommentList/Comment/Comment.jsx
+++ client/app/bundles/comments/components/CommentBox/CommentList/Comment/Comment.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { marked } from 'marked';
+import { gfmHeadingId } from 'marked-gfm-heading-id';
+import { mangle } from 'marked-mangle';
 import sanitizeHtml from 'sanitize-html';
 import css from './Comment.module.scss';
+
+marked.use(gfmHeadingId());
+marked.use(mangle());
 
 const Comment = React.forwardRef((props, ref) => {
   const { author, text } = props;
