edf1a4849ca2e1bf2c42ef7894fc08019a53ef02
Mostafa Ahangarha <ahangarha@riseup.net>
2023-09-08 15:52:40 +0330

    Improve navbar

--- client/app/bundles/comments/components/NavigationBar/NavigationBar.jsx
+++ client/app/bundles/comments/components/NavigationBar/NavigationBar.jsx
@@ -3,7 +3,7 @@
 
 import classNames from 'classnames';
 import _ from 'lodash';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import CommentsCount from './CommentsCount.jsx';
@@ -12,52 +12,101 @@ import * as paths from '../../constants/paths';
 function NavigationBar(props) {
   const { commentsCount, pathname } = props;
 
-  /* eslint-disable new-cap */
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menuWrapperClasses = 'flex flex-col lg:flex-row flex-wrap lg:items-center lg:visible';
+
   return (
-    <nav className="navbar navbar-default">
-      <div className="container">
-        <div className="navbar-header">
-          <button
-            type="button"
-            className="navbar-toggle"
-            data-toggle="collapse"
-            data-target="#bs-example-navbar-collapse-1"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-          </button>
-          <a className="navbar-brand" href="http://www.shakacode.com">
-            ShakaCode
-          </a>
-        </div>
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav">
+    <nav className="bg-yellow-50">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between">
+          <div className="flex justify-between items-center align-middle">
+            <a className="p-4" href="http://www.shakacode.com">
+              ShakaCode
+            </a>
+
+            <button
+              type="button"
+              className="navbar-toggle"
+              data-toggle="collapse"
+              data-target="#bs-example-navbar-collapse-1"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <div className="p-4 lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </div>
+            </button>
+          </div>
+
+          <ul className={`${menuWrapperClasses} ${isOpen ? '' : ' collapse'}`}>
             <li
               className={classNames({
-                active: pathname === paths.ROUTER_PATH || pathname === paths.REACT_ROUTER_PATH,
+                'bg-yellow-100': pathname === paths.ROUTER_PATH || pathname === paths.REACT_ROUTER_PATH,
               })}
             >
-              <a href={paths.ROUTER_PATH}>React Router Demo</a>
+              <a
+                className="px-2 py-4 inline-block text-gray-500 hover:text-gray-700"
+                href={paths.ROUTER_PATH}
+              >
+                React Router Demo
+              </a>
             </li>
-            <li className={classNames({ active: pathname === paths.NO_ROUTER_PATH })}>
-              <a href={paths.NO_ROUTER_PATH}>React Demo</a>
+            <li className={classNames({ 'bg-yellow-100': pathname === paths.NO_ROUTER_PATH })}>
+              <a
+                className="px-2 py-4 inline-block text-gray-500 hover:text-gray-700"
+                href={paths.NO_ROUTER_PATH}
+              >
+                React Demo
+              </a>
             </li>
-            <li className={classNames({ active: pathname === paths.SIMPLE_REACT_PATH })}>
-              <a href={paths.SIMPLE_REACT_PATH}>Simple React</a>
+            <li className={classNames({ 'bg-yellow-100': pathname === paths.SIMPLE_REACT_PATH })}>
+              <a
+                className="px-2 py-4 inline-block text-gray-500 hover:text-gray-700"
+                href={paths.SIMPLE_REACT_PATH}
+              >
+                Simple React
+              </a>
             </li>
-            <li className={classNames({ active: pathname === paths.STIMULUS_PATH })}>
-              <a href={paths.STIMULUS_PATH}>Stimulus Demo</a>
+            <li className={classNames({ 'bg-yellow-100': pathname === paths.STIMULUS_PATH })}>
+              <a
+                className="px-2 py-4 inline-block text-gray-500 hover:text-gray-700"
+                href={paths.STIMULUS_PATH}
+              >
+                Stimulus Demo
+              </a>
             </li>
-            <li className={classNames({ active: pathname === paths.RAILS_PATH })}>
-              <a href={paths.RAILS_PATH}>Classic Rails</a>
+            <li className={classNames({ 'bg-yellow-100': pathname === paths.RAILS_PATH })}>
+              <a className="px-2 py-4 inline-block text-gray-500 hover:text-gray-700" href={paths.RAILS_PATH}>Classic Rails</a>
             </li>
             <li>
-              <a href="https://github.com/shakacode/react-webpack-rails-tutorial">Source</a>
+              <a
+                className="px-2 py-4 inline-block text-gray-500 hover:text-gray-700"
+                href="https://github.com/shakacode/react-webpack-rails-tutorial"
+              >
+                Source
+              </a>
             </li>
             <li>
-              <a href="https://forum.shakacode.com/c/reactjs">Forum</a>
+              <a
+                className="px-2 py-4 inline-block text-gray-500 hover:text-gray-700"
+                href="https://forum.shakacode.com/c/reactjs"
+              >
+                Forum
+              </a>
             </li>
             {_.isNumber(commentsCount) && CommentsCount({ commentsCount })}
           </ul>
