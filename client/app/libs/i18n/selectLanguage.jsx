ada3549f9cb18d988647c5a7e470e8a2eb7dbdb3
Mostafa Ahangarha <ahangarha@riseup.net>
2023-09-10 22:23:46 +0330

    Improve language selector style

--- client/app/libs/i18n/selectLanguage.jsx
+++ client/app/libs/i18n/selectLanguage.jsx
@@ -4,7 +4,11 @@ import { defaultLocale } from './default';
 
 function SelectLanguage(onChange, locale = defaultLocale) {
   return (
-    <select onChange={(e) => onChange(e.target.value)} value={locale}>
+    <select
+      onChange={(e) => onChange(e.target.value)}
+      value={locale}
+      className="px-3 py-1 rounded cursor-pointer border border-gray-400 hover:bg-gray-300"
+    >
       <option value="en">English</option>
       <option value="de">Deutsch</option>
       <option value="ja">日本語</option>
