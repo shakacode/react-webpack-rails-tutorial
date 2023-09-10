57c45db8b283c014f792f02bde37e4f18f62a4bd
Mostafa Ahangarha <ahangarha@riseup.net>
2023-09-10 22:19:15 +0330

    Improve footer style

--- client/app/bundles/comments/components/Footer/Footer.jsx
+++ client/app/bundles/comments/components/Footer/Footer.jsx
@@ -5,18 +5,20 @@ import css from './Footer.module.scss';
 export default class Footer extends BaseComponent {
   render() {
     return (
-      <div className="container mx-auto">
-        <a href="http://www.shakacode.com">
-          <h3>
-            <div className={css.logo} />
-            Example of styling using image-url and Open Sans Light custom font
-          </h3>
-        </a>
-        <a href="https://twitter.com/railsonmaui">
-          <div className={css.twitterImage} />
-          Rails On Maui on Twitter
-        </a>
-      </div>
+      <footer className=" text-neutral-200 bg-[#222] py-8">
+        <div className="container mx-auto px-4">
+          <a href="http://www.shakacode.com">
+            <h3>
+              <div className={css.logo} />
+              Example of styling using image-url and Open Sans Light custom font
+            </h3>
+          </a>
+          <a href="https://twitter.com/railsonmaui">
+            <div className={css.twitterImage} />
+            Rails On Maui on Twitter
+          </a>
+        </div>
+      </footer>
     );
   }
 }
