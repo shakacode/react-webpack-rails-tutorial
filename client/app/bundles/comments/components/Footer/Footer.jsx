f37183021be1312a00b3685ab8502edf020fa284
Mostafa Ahangarha <ahangarha@riseup.net>
2023-09-11 22:32:35 +0330

    Use tailwind for image background in footer

--- client/app/bundles/comments/components/Footer/Footer.jsx
+++ client/app/bundles/comments/components/Footer/Footer.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import BaseComponent from 'libs/components/BaseComponent';
-import css from './Footer.module.scss';
 
 export default class Footer extends BaseComponent {
   render() {
     return (
-      <footer className=" text-neutral-200 bg-[#222] py-8">
+      <footer className=" text-neutral-200 bg-[#222] py-8 mt-16">
         <div className="container mx-auto px-4">
           <a href="http://www.shakacode.com">
-            <h3>
-              <div className={css.logo} />
+            <h3 className="flex gap-4 items-center">
+              <div className="w-[146px] h-[40px] bg-[url('../images/railsonmaui.png')]" />
               Example of styling using image-url and Open Sans Light custom font
             </h3>
           </a>
-          <a href="https://twitter.com/railsonmaui">
-            <div className={css.twitterImage} />
+          <a href="https://twitter.com/railsonmaui" className="flex gap-4 items-center">
+            <div className="w-16 h-16 bg-[url('../images/twitter_64.png')]" />
             Rails On Maui on Twitter
           </a>
         </div>
