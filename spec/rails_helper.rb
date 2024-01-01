599b2bc2b440cbd368071da954c0851be520387a
Mostafa Ahangarha <ahangarha@riseup.net>
2024-01-01 20:49:17 +0330

    Comment back capybara default max wait time config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -104,8 +104,7 @@ RSpec.configure do |config|
                          options: { args: %w[headless disable-gpu no-sandbox disable-dev-shm-usage] }
   end
 
-  Capybara.default_max_wait_time = 15
-
+  # Capybara.default_max_wait_time = 15
   puts "=" * 80
   puts "Capybara using driver: #{Capybara.javascript_driver}"
   puts "=" * 80
