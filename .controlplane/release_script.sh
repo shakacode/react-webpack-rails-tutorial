e1aa3fc7284165ac49f47479c33e3650644e10c3
Justin Gordon <justin@shakacode.com>
2024-03-24 22:18:27 -1000

    test error handling

--- .controlplane/release_script.sh
+++ .controlplane/release_script.sh
@@ -1,8 +1,25 @@
 #!/bin/bash -e
 
-echo 'Running release_script.sh per controlplane.yml'
 
-echo 'Run DB migrations'
-./bin/rails db:prepare
+log() {
+    echo "[`date +%Y-%m-%d:%H:%M:%S`]: $1"
+}
 
-echo 'Completed release_script.sh per controlplane.yml'
+error_exit() {
+    log "$1" 1>&2
+    exit 1
+}
+
+# Test error!
+error_exit
+
+log 'Running release_script.sh per controlplane.yml'
+
+if [ -x ./bin/rails ]; then
+    log 'Run DB migrations'
+    ./bin/rails db:prepare || error_exit "Failed to run DB migrations"
+else
+    error_exit "./bin/rails does not exist or is not executable"
+fi
+
+log 'Completed release_script.sh per controlplane.yml'
