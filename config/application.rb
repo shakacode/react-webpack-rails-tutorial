31270c45f67259cd1b6655738f58c720da7388a8
ihab4real <ihabadham4p@gmail.com>
2024-11-26 00:44:20 +0200

    upgrade rails from 7.1 to 7.2
    it's recommended to upgrade to the next major version instead of jumping directly to v8.0

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "boot"
 
 require "rails/all"
@@ -13,10 +11,13 @@ module RailsReactTutorial
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
-
-    config.action_cable.allowed_request_origins = [Rails.application.credentials.action_cable_url]
-
+    #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
@@ -11,4 +9,4 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
