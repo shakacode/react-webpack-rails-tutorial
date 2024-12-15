b88ffab5429892711a5cf5aaaf6966eac452e725
ihab4real <ihabadham4p@gmail.com>
2024-12-15 21:20:11 +0200

    avoid deprecation warning "`to_time` will always preserve the full timezone rather than offset of the receiver in Rails 8.1."
    Configure Rails to use full timezone preservation instead of offset, adopting the upcoming Rails 8.1 default behavior early.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module RailsReactTutorial
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
+
+    config.active_support.to_time_preserves_timezone = :zone
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
