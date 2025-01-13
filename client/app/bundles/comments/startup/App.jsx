a789648c0fef382bbdb0cd5969633b061f96de4a
Ihab Adham <71561048+ihab4real@users.noreply.github.com>
2025-01-13 07:36:47 +0200

    add locale data imports for react-intl (#609)

--- client/app/bundles/comments/startup/App.jsx
+++ client/app/bundles/comments/startup/App.jsx
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactOnRails from 'react-on-rails';
 
 import NonRouterCommentsContainer from '../containers/NonRouterCommentsContainer.jsx';
+import 'intl/locale-data/jsonp/en';
+import 'intl/locale-data/jsonp/de';
+import 'intl/locale-data/jsonp/ja';
+import 'intl/locale-data/jsonp/zh';
 
 function App(_props, _railsContext) {
   const store = ReactOnRails.getStore('commentsStore');
