db6d4dc6e3f6382e1eefe069cd1da865727e734d
Justin Gordon <justin@shakacode.com>
2025-01-21 21:33:09 -1000

    per code review

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,10 +35,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # config.assume_ssl = true
+  config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
