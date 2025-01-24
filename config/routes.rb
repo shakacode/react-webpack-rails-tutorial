8182ec43321c29fc43ef92b159ddce2d8d978406
Muhammad Umar <mohammadumar28@gmail.com>
2025-01-24 10:53:59 +0200

    Add Authication API

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
 
   root "pages#index"
+
+  namespace :api do
+    post 'login', to: 'authentication#create'
+  end
 
   get "simple", to: "pages#simple"
   get "rescript", to: "pages#rescript"
