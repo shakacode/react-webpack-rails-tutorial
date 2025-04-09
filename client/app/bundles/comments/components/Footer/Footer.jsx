7b05d8dba9977090cdf7d555b95be640ce48c932
Judah Meek <judah.meek@gmail.com>
2025-04-09 02:14:32 -0500

    test

--- client/app/bundles/comments/components/Footer/Footer.jsx
+++ client/app/bundles/comments/components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ export default class Footer extends BaseComponent {
           </a>
           <a href="https://twitter.com/railsonmaui" className="flex gap-4 items-center">
             <div className="w-16 h-16 bg-[url('../images/twitter_64.png')]" />
-            Rails On Maui on X/Twitter
+            Rails On Maui on X (Twitter)
           </a>
         </div>
       </footer>
