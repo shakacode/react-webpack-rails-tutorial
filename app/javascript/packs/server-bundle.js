c1d8c6377351cb1538aa1ae83a009d65a6c73908
Justin Gordon <justin@shakacode.com>
2025-09-25 18:35:45 -1000

    Add auto-generated server bundle import

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,1 +1,3 @@
+// import statement added by react_on_rails:generate_packs rake task
+import "./../generated/server-bundle-generated.js"
 // Placeholder comment - auto-generated imports will be prepended here by react_on_rails:generate_packs
